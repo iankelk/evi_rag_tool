965f1899da11c9be500bb4231ef3979592d3ec62
Ian Kelk <iankelk@gmail.com>
2024-07-18 11:57:21 -0400

    Initial commit

---
+++ src/api_service/api/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Google Cloud Bucket Name
+GCP_BUCKET_NAME = "rag_scraper_bucket"
+
+# Current Weaviate IP
+WEAVIATE_IP_ADDRESS = "34.145.246.242"
+
+# Current Weaviate port
+WEAVIATE_PORT = "8080"
